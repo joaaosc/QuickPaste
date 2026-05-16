7e0f79f2d558f67a0e7397bceeeb38e23b1a931f
joaaosc <joaocosta29@poli.ufrj.br>
2026-05-16 18:18:49 -0300

    Initial Commit

---
+++ QuickPaste/QuickPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPasteApp.swift
+//  QuickPaste
+//
+//  Created by João Pedro Torres on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
